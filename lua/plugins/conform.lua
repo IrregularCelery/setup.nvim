956e344480658ebcf99d42a6fb095699d358d8cf
Mohsen Rad <irregular.celery@gmail.com>
2024-08-03 14:12:43 +0330

    Add more formatters in conform plugin

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,14 +1,41 @@
 return { -- Autoformat
-    "stevearc/conform.nvim",
-    lazy = false,
-    keys = {
-        {
-            "<leader>f",
-            function()
-                require("conform").format { async = true, lsp_fallback = true }
-            end,
-            mode = "",
-            desc = "[F]ormat buffer",
-        },
-    },
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				go = { "goimports" },
+				rust = { "rustfmt" },
+				python = { "isort", "black" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				svelte = { "prettier" },
+				bash = { "beautysh" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				toml = { "prettier" },
+				["*"] = { "codespell" },
+				["_"] = { "trim_whitespace" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>f", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end)
+	end,
 }
