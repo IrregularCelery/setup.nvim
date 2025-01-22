bd73db6710d780fd8f68553d54adc62086cf3732
Mohsen Rad <irregular.celery@gmail.com>
2025-01-23 00:51:48 +0330

    Improve "CommentDivider" helper function

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,17 +1,23 @@
 -- ----------------------------- Comment Divider ---------------------------- --
 
+local devider_max_length = 80
+
 local function create_divider(text)
-	local width = vim.o.textwidth > 0 and vim.o.textwidth or 80
+	local width = vim.o.textwidth > 0 and math.min(vim.o.textwidth, devider_max_length) or 80
+
+	if width % 2 ~= 0 then
+		width = width - 1
+	end
 
 	local commentstring = vim.bo.commentstring or "//%s//" -- Default
 	local comment_start, comment_end = commentstring:match("^(.-)%%s(.-)$")
-
-	local divider_length = width - #comment_start - #comment_end
 
 	if not comment_start then -- Handle cases where %s is not present or regex fails
 		comment_start = "//"
 		comment_end = ""
 	end
+
+	local divider_length = width - #comment_start or 0 - #comment_end or 0
 
 	if #comment_end < 1 then
 		comment_end = string.reverse(comment_start)
@@ -24,7 +30,7 @@ local function create_divider(text)
 	local padding = math.floor((divider_length - #text) / 2)
 	local divider = comment_start
 		.. " "
-		.. string.rep("-", padding - 2) -- Accounting for the spaces
+		.. string.rep("-", padding - 3) -- Accounting for the spaces
 		.. " "
 		.. text
 		.. " "
