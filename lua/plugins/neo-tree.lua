a91f206dfae6624e4de095910874e3e543e53f5d
Mohsen Rad <irregular.celery@gmail.com>
2025-02-12 18:57:43 +0330

    Plugin `neo-tree`: Add an event to auto-close when a file was opened

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,21 +1,32 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        require("neo-tree").setup({
-            window = {
-                position = "left",
-                width = 30
-            }
-        })
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			window = {
+				position = "left",
+				width = 30,
+				mappings = {
+					["<space>"] = "open",
+				},
+			},
+			event_handlers = {
+				{
+					-- Close neotree on `file_opened`
+					event = "file_opened",
+					handler = function()
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+		})
 
-        vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal<CR>", {})
-        vim.keymap.set("n", "<leader>m", ":Neotree filesystem close<CR>", {})
-    end
+		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal<CR>", {})
+		vim.keymap.set("n", "<leader>m", ":Neotree filesystem close<CR>", {})
+	end,
 }
-
