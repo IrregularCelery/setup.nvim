a58a274f429ec3f779f1799328ba90332c64856c
Mohsen Rad <irregular.celery@gmail.com>
2025-02-22 23:03:22 +0330

    Plugin `completions`: Switch to `blink.cmp`

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,38 +1,17 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
+	"saghen/blink.cmp",
+	dependencies = "rafamadriz/friendly-snippets",
+	version = "*",
+	opts = {
+		keymap = { preset = "default" },
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "mono",
+		},
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+		signature = { enabled = true },
 	},
-	{
-		"hrsh7th/cmp-path",
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-
-			cmp.setup({
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "path" },
-					{ name = "crates" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
+	opts_extend = { "sources.default" },
 }
