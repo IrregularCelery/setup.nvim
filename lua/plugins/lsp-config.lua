564b87d455bb1b9d99d75a53c55fcf02e9354cd0
Mohsen Rad <irregular.celery@gmail.com>
2025-05-21 12:33:15 +0330

    Add setup config to `lua_ls` for `lsp-config`

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -32,6 +32,33 @@ return {
 
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
+				on_init = function(client)
+					if client.workspace_folders then
+						local path = client.workspace_folders[1].name
+
+						if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+							return
+						end
+					end
+
+					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+						runtime = {
+							version = "LuaJIT",
+						},
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME,
+								-- "${3rd}/luv/library"
+								-- "${3rd}/busted/library",
+							},
+							-- library = vim.api.nvim_get_runtime_file("", true)
+						},
+					})
+				end,
+				settings = {
+					Lua = {},
+				},
 			})
 
 			lspconfig.jdtls.setup({})
