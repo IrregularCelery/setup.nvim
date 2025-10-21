6cbbdcd0b2e839924d7a8b69b012a7132784aa89
Mohsen Rad <irregular.celery@gmail.com>
2025-10-21 04:47:51 +0330

    Revert back to using `conform` for the formatter

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,92 +1,55 @@
 return {
-  -- LSP diagnostics and formatting injector
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    { "nvimtools/none-ls-extras.nvim" },
-    { "jayp0521/mason-null-ls.nvim" },
-    { "gbprod/none-ls-shellcheck.nvim" },
-  },
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
   config = function()
-    local null_ls = require("null-ls")
-    local formatting = null_ls.builtins.formatting
-    local diagnostics = null_ls.builtins.diagnostics
-
-    local clang_format = formatting.clang_format
-    local black = formatting.black
-    local isort = formatting.isort
-    local stylua = formatting.stylua
-    local prettier = formatting.prettier
-    local eslint_d = require("none-ls.diagnostics.eslint_d")
-    local sqlfluff_fmt = formatting.sqlfluff
-    local sqlfluff_diag = diagnostics.sqlfluff
-    local shfmt = formatting.shfmt
-    local shellcheck = require("none-ls-shellcheck.diagnostics")
-    local checkmake = diagnostics.checkmake
-    local markdownlint = diagnostics.markdownlint
-
-    require("mason-null-ls").setup({
-      ensure_installed = {
+    require("conform").setup({
+      formatters_by_ft = {
         -- Systems languages
-        "clang_format", -- C/C++
+        rust = { "rustfmt" },
+        c = { "clang_format" },
+        cpp = { "clang_format" },
 
         -- High-level languages
-        "black", -- Python formatting
-        "isort", -- Python imports
+        go = { "goimports" },
+        python = { "isort", "black" },
 
         -- Lua
-        "stylua", -- Lua
+        lua = { "stylua" },
 
         -- Web development
-        "prettier", -- JS/TS/HTML/CSS/JSON/YAML/Markdown/GraphQL
-        "eslint_d", -- JS/TS linting
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        scss = { "prettier" },
+        html = { "prettier" },
 
         -- Config/Data formats
-        "sqlfluff", -- SQL
+        json = { "prettier" },
+        jsonc = { "prettier" },
+        yaml = { "prettier" },
+        sql = { "sqlfluff" },
+        graphql = { "prettier" },
 
         -- Shell/Scripting
-        "shfmt", -- Bash formatting
-        "shellcheck", -- Bash linting
-
-        -- Build tools
-        "checkmake", -- Makefile
+        sh = { "shfmt" },
+        bash = { "shfmt" },
 
         -- Documentation
-        "markdownlint", -- Markdown
+        markdown = { "prettier" },
+
+        -- Fallback
+        ["_"] = { "trim_whitespace" },
       },
-      automatic_installation = true,
-    })
 
-    local sources = {
-      clang_format,
-      black,
-      isort,
-      stylua,
-      prettier,
-      eslint_d,
-      sqlfluff_fmt,
-      sqlfluff_diag,
-      shfmt,
-      shellcheck,
-      checkmake,
-      markdownlint,
-    }
-
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-    null_ls.setup({
-      sources = sources,
-      on_attach = function(client, bufnr)
-        if client:supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ async = false })
-            end,
-          })
-        end
-      end,
+      -- Format on save
+      format_on_save = {
+        timeout_ms = 1000,
+        async = false,
+        lsp_fallback = true,
+      },
     })
   end,
 }
